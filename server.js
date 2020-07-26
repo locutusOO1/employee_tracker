a2ca0dc8c845d8341bb4007308a0be1996d9af6f
Clark Kent with glasses <61812163+locutusOO1@users.noreply.github.com>
2020-07-26 13:18:15 -0500

    added view functions

--- server.js
+++ server.js
@@ -38,8 +38,14 @@ function initPrompt () {
             case "Add Role":
             case "Add Employee":
             case "View Departments":
+                viewDepts();
+                break;
             case "View Roles":
+                viewRoles();
+                break;
             case "View Employees":
+                viewEmployees();
+                break;
             case "Update Employee Role":
             case "Update Employee Manager":
             case "View Employees by Manager":
@@ -47,10 +53,53 @@ function initPrompt () {
             case "Delete Role":
             case "Delete Employee":
             case "View Total Utilized Budget by Department":
+                viewBudgetByDept();
+                break;
             case "Exit":
             default:
                 connection.end();
                 process.exit();
         }
+    });
+}
+
+function viewDepts() {
+    connection.query("select * from department",(err, results) => {
+        if (err) throw err;
+        console.table(results);
+        initPrompt();
+    });
+}
+
+function viewRoles() {
+    connection.query("select * from role",(err, results) => {
+        if (err) throw err;
+        console.table(results);
+        initPrompt();
+    });
+}
+
+function viewEmployees() {
+    connection.query("select * from employee",(err, results) => {
+        if (err) throw err;
+        console.table(results);
+        initPrompt();
+    });
+}
+
+function viewBudgetByDept() {
+    connection.query(
+        `select ifnull(d.name,"*No Dept") as Department,
+            concat('$',format(sum(r.salary),2)) as Budget,
+            count(1) as "Number of Employees",
+            concat('$',format((sum(r.salary)/count(1)),2)) as "Avg Cost Per Employee"
+        from employee e
+        inner join role r on (e.role_id = r.id)
+        left join department d on (r.department_id = d.id)
+        group by ifnull(d.name,"*No Dept")
+        order by ifnull(d.name,"*No Dept")`,(err, results) => {
+        if (err) throw err;
+        console.table(results);
+        initPrompt();
     });
 }
